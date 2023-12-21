934105acaa79feee698b5854d214e371f6d3a5e8
vanshpatelx <vanshpatel1299@gmail.com>
2023-12-22 00:41:12 +0530

    JWT Middleware Done

---
+++ src/middleware/JWTMiddleware.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+
+const authenticateJWT = (req, res, next) => {
+    const token = req.header('Authorization');
+  
+    if (!token) {
+      return res.sendStatus(401);
+    }
+  
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+      if (err) {
+        return res.sendStatus(403);
+      }
+  
+      req.user = user;
+      next();
+    });
+  };
+
+module.exports = authenticateJWT ;
